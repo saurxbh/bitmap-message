c7d7ea9f5313b11a77472ec38fd25c15a622076c
saurxbh <36542272+saurxbh@users.noreply.github.com>
2024-03-08 23:34:01 -0500

    Display message in bitmap format

---
+++ bitmap-message.py
@@ -0,0 +1,44 @@
+import sys
+
+bitmap = '''
+....................................................................
+   **************   *  *** **  *      ******************************
+  ********************* ** ** *  * ****************************** *
+ **      *****************       ******************************
+          *************          **  * **** ** ************** *
+           *********            *******   **************** * *
+            ********           ***************************  *
+   *        * **** ***         *************** ******  ** *
+               ****  *         ***************   *** ***  *
+                 ******         *************    **   **  *
+                 ********        *************    *  ** ***
+                   ********         ********          * *** ****
+                   *********         ******  *        **** ** * **
+                   *********         ****** * *           *** *   *
+                     ******          ***** **             *****   *
+                     *****            **** *            ********
+                    *****             ****              *********
+                    ****              **                 *******   *
+                    ***                                       *    *
+                    **     *                    *
+....................................................................
+'''
+
+print('Bitmap message')
+print('Enter the message to display with the bitmap.')
+message = input('> ')
+
+if message == '':
+    sys.exit()
+
+# Loop over each line in the bitmap
+for line in bitmap.splitlines():
+    # Loop over each character in the line
+    for i, bit in enumerate(line):
+        if bit == ' ':
+            # Print an empty space since there's an empty space in the bitmap
+            print(' ', end='')
+        else:
+            # Print a character from the message
+            print(message[i % len(message)], end='')
+    print() # Print a newline
